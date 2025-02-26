2f40c56b18d405cc194e915f5d5208fe59967091
Subhash Kumar <subhashkushwah134@gmail.com>
2025-02-26 23:56:52 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import { ThemeProvider } from "./components/ThemeProvider"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
-import Experience from "./components/experience"
+import Experience from "./components/Experience"
 import Education from "./components/Education"
-import Projects from "./components/projects"
-import Contact from "./components/contact"
-import Footer from "./components/footer"
-
-
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 function App() {
   return (
